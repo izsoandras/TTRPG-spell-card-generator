ea7a312a57b5ca76ce72a4178a4ec81928d09a00
Izsó András <izso.andras@simonyi.bme.hu>
2024-11-04 13:29:29 +0100

    Make concatenation more robust

--- concat_output.py
+++ concat_output.py
@@ -1,25 +1,27 @@
 
 import win32com.client
 import os
-from os import listdir
-from os.path import isfile, join
 
-mypath = './output'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# gather files to merge
+mypath = os.path.join(os.getcwd(), 'output')
+onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
+# create base output presentation
 powerPoint = win32com.client.Dispatch("PowerPoint.Application")
-outputPresentation = powerPoint.Presentations.Open(onlyfiles[0])
+outputPresentation = powerPoint.Presentations.Open(os.path.join(mypath, onlyfiles[0]))
 windowRef = outputPresentation.Application.Windows(1)
-outputPresentation.SaveAs("output.pptx")
+outputPresentation.SaveAs(mypath+"/merged.pptx")
 
+# insert every presentation into the output one
 for name in onlyfiles[1:]:
-    currentPresentation = powerPoint.Presentations.Open(name)
+    currentPresentation = powerPoint.Presentations.Open(os.path.join(mypath, name))
     currentPresentation.Slides.Range(range(1, currentPresentation.Slides.Count + 1)).copy()
     windowRef.Activate()
     outputPresentation.Application.CommandBars.ExecuteMso("PasteSourceFormatting")
     currentPresentation.Close()
 
+# save the output presentation and export to pdf
 outputPresentation.save()
-outputPresentation.saveAs("output.pdf", 32)
+outputPresentation.saveAs(os.path.join(mypath, "merged.pdf"), 32)
 outputPresentation.close()
 powerPoint.Quit()
